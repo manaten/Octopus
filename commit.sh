174dcfdaf3ad47224c9fdadbd7e0c5b3ba4d230e
mkato6 <mkato6@muska.is.titech.ac.jp>
2012-01-10 22:07:41 +0900

    Modify octopus to remove tempolary files.

---
+++ commit.sh
@@ -0,0 +1,2 @@
+scp -r ../Octopus wakitalab@pom:.
+ssh wakitalab@pom chmod -R 777 Octopus
--- test/djsTest/hi_order_func/server.js
+++ test/djsTest/hi_order_func/server.js
@@ -2,4 +2,3 @@ var count = 0;
 exports.printServer = function() {
 	console.log("server said, Hello! :" + (count++));
 };
-console.log(require.paths);
