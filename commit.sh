e1122231b4d6dbdd8628213c0835e82e09f14acd
mkato6 <mkato6@muska.is.titech.ac.jp>
2012-01-11 15:08:26 +0900

    Change texlipse settings.

--- commit.sh
+++ commit.sh
@@ -1,2 +1,6 @@
-scp -r ../Octopus wakitalab@pom:.
+scp -r ./node_modules wakitalab@pom:~/Octopus
+scp -r ./translator wakitalab@pom:~/Octopus
+scp -r ./test wakitalab@pom:~/Octopus
+scp -r ./sample wakitalab@pom:~/Octopus
+scp -r ./octopus wakitalab@pom:~/Octopus
 ssh wakitalab@pom chmod -R 777 Octopus
