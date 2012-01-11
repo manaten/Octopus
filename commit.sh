c5dded39c294e843009374cde97b21a1082799f0
mkato6 <mkato6@muska.is.titech.ac.jp>
2012-01-11 17:21:11 +0900

    hoge

--- commit.sh
+++ commit.sh
@@ -1,6 +1,2 @@
-scp -r ./node_modules wakitalab@pom:~/Octopus
-scp -r ./translator wakitalab@pom:~/Octopus
-scp -r ./test wakitalab@pom:~/Octopus
-scp -r ./sample wakitalab@pom:~/Octopus
-scp -r ./octopus wakitalab@pom:~/Octopus
+scp -r ./node_modules ./translator ./test ./sample ./octopus wakitalab@pom:~/Octopus
 ssh wakitalab@pom chmod -R 777 Octopus
