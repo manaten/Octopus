ee3c65e78576786b231f136503dc4b6a85b2227e
mkato6 <mkato6@muska.is.titech.ac.jp>
2012-01-23 16:33:05 +0900

    Perhaps ok delegation system.

--- test/djsTest/delegation/client.js
+++ test/djsTest/delegation/client.js
@@ -1,6 +1,4 @@
-var clientClass = function(prop) {
-	this.prop = "I am client class !!";
-};
+var clientClass = function(prop) { this.prop = "I am client class !!"; };
 clientClass.prototype = new (Octopus.getServer().serverClass)("I am prototype!");
 var clientClass2 = function(prop) { this.prop = prop; };
 clientClass2.prototype.method = Octopus.getServer().serverMethod;
@@ -9,9 +7,8 @@ var ins1 = new clientClass("");
 var ins2 = new clientClass2("I'm client obj !!!");
 var ins3 = new (Octopus.getServer().serverClass)("Initialized from client !");
 
-console.log(ins1.constructor === clientClass);
-console.log(ins1.constructor);
-console.log(ins2.constructor === clientClass2);
+console.log(ins1 instanceof clientClass);
+console.log(ins2 instanceof clientClass2);
 
 ins1.method();
 ins1.prop = "I'm decl in client!!!";
